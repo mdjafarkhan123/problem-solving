fc3df27fca9a11cd2c63d9ede2bfa1584b46afe5
Jafar Khan <profile.mdjafarkhan@gmail.com>
2024-07-28 09:52:55 +0600

    Added a class template

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-let ob = {
-    name: "Jafar",
-    age: 26,
-};
+class Users {
+    constructor(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
 
-console.log(ob);
+    fullName = () => this.firstName + " " + this.lastName;
+}
+
+let user1 = new Users("Jafar", "Khan", 20);
+console.log(user1.fullName());
