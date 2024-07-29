6fa0a5c9199ae34298bf78cea029f7e1f98b1a95
Jafar Khan <profile.mdjafarkhan@gmail.com>
2024-07-29 12:08:33 +0600

    Task 4

---
+++ 4. Calculate the total price from  objects of array/index.js
@@ -0,0 +1,42 @@
+/* 
+Task: calculate the total price of groceries
+
+Create a function that takes an array of objects (groceries) which calculates the total price and returns it as a number. A grocery object has a product, a quantity and a price, for example:
+
+{
+  "product": "Milk",
+  "quantity": 1,
+  "price": 1.50
+}
+*/
+
+
+function getTotalPrice(arr) {
+    let result = arr.reduce((total, item) => {
+        total += item.price * item.quantity;
+        return total;
+    }, 0);
+
+    console.log(Number.parseFloat(result.toFixed(2)));
+}
+
+getTotalPrice([{ product: "Milk", quantity: 1, price: 1.5 }]); //➞ 1.5
+
+getTotalPrice([
+    { product: "Milk", quantity: 1, price: 1.5 },
+    { product: "Cereals", quantity: 1, price: 2.5 },
+]); //➞ 4
+
+getTotalPrice([{ product: "Milk", quantity: 3, price: 1.5 }]); //➞ 4.5
+
+getTotalPrice([
+    { product: "Milk", quantity: 1, price: 1.5 },
+    { product: "Eggs", quantity: 12, price: 0.1 },
+    { product: "Bread", quantity: 2, price: 1.6 },
+    { product: "Cheese", quantity: 1, price: 4.5 },
+]); //➞ 10.4
+
+getTotalPrice([
+    { product: "Chocolate", quantity: 1, price: 0.1 },
+    { product: "Lollipop", quantity: 1, price: 0.2 },
+]); //➞ 0.3
