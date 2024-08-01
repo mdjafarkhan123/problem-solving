f256d42b1ff668c3e22adc4d8273a37c2a240e9c
Jafar Khan <profile.mdjafarkhan@gmail.com>
2024-08-01 10:16:22 +0600

    Frequency destribution

---
+++ 10. Frequency destribution/index.js
@@ -0,0 +1,38 @@
+/*
+
+Create a function that returns the frequency distribution of an array. This function should return an object, where the keys are the unique elements and the values are the frequency in which those elements occur.
+
+Examples
+getFrequencies(["A", "B", "A", "A", "A"]) ➞ { A: 4, B: 1 }
+
+getFrequencies([1, 2, 3, 3, 2]) ➞ { "1": 1, "2": 2, "3": 2 }
+
+getFrequencies([true, false, true, false, false]) ➞ { true: 2, false: 3 }
+
+getFrequencies([]) ➞ {}
+Notes
+If given an empty array, return an empty object (see example #4).
+The object should be in the same order as in the input array.
+*/
+
+function getFrequencies(arr) {
+    let ob = {};
+    let arrayItem = [...new Set(arr)];
+    for (let value of arrayItem) {
+        let occurance = arr.filter(function (item) {
+            return item == value;
+        });
+
+        ob[value] = occurance.length;
+    }
+
+    console.log(ob);
+}
+
+getFrequencies(["A", "B", "A", "A", "A"]); //➞ { A: 4, B: 1 }
+
+getFrequencies([1, 2, 3, 3, 2]); // ➞ { "1": 1, "2": 2, "3": 2 }
+
+getFrequencies([true, false, true, false, false]); //➞ { true: 2, false: 3 }
+
+getFrequencies([]); //➞ {}
