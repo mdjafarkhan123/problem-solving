0cf8f1becd11072bf7c66bf4fae2e3a636189178
Jafar Khan <profile.mdjafarkhan@gmail.com>
2024-08-02 11:20:13 +0600

    logic, loops, recursion, string

---
+++ 13. Count the maximum number of swapping in binary value/index.js
@@ -0,0 +1,33 @@
+/*
+Write a function that returns the minimum number of swaps to convert the first binary string into the second.
+
+Examples
+minSwaps("1100", "1001") ➞ 1
+
+minSwaps("110011", "010111") ➞ 1
+
+minSwaps("10011001", "01100110") ➞ 4
+Notes
+Both binary strings will be of equal length.
+Both binary strings will have an equal number of zeroes and ones.
+A swap is switching two elements in a string (swaps do not have to be adjacent
+*/
+
+
+"use strict";
+function minSwaps(a, b) {
+    let minSwap = 0;
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] == 1 && a[i] != b[i]) {
+            minSwap++;
+        }
+    }
+
+    console.log(minSwap);
+}
+
+minSwaps("1100", "1001"); //➞ 1
+
+minSwaps("110011", "010111"); //➞ 1
+
+minSwaps("10011001", "01100110"); //➞ 4
