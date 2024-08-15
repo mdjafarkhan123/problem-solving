49e4a0b43a76ce2e5571bd4c1b1b0768c133fe51
Jafar Khan <profile.mdjafarkhan@gmail.com>
2024-08-15 10:05:21 +0600

    arrays, objects, sorting

---
+++ 22. The frugal gentleman/index.js
@@ -0,0 +1,45 @@
+/*
+Atticus has been invited to a dinner party, and he decides to purchase a bottle of wine. However, he has little knowledge of how to choose a good bottle. Being a very frugal gentleman (yet disliking looking like a cheapskate), he decides to use a very simple rule. In any selection of two or more wines, he will always buy the second-cheapest.
+
+Given an array of wine objects, write a function that returns the name of the wine he will buy for the party. If given an empty array, return null. If given an array of only one, Atticus will buy that wine.
+
+Examples
+chosenWine([
+  { name: "Wine A", price: 8.99 },
+  { name: "Wine 32", price: 13.99 },
+  { name: "Wine 9", price: 10.99 }
+]) ➞ "Wine 9"
+
+chosenWine([{ name: "Wine A", price: 8.99 }]) ➞ "Wine A"
+
+chosenWine([]) ➞ null
+Notes
+All wines will be different prices, so there is no confusion in the ordering.
+
+*/
+
+function chosenWine(arr) {
+    if (arr.length == 0) {
+        console.log(null);
+        return;
+    }
+    if (arr.length == 1) {
+        console.log(arr[0].name);
+        return;
+    }
+    arr.sort((a, b) => {
+        return a.price - b.price;
+    });
+
+    console.log(arr[1].name);
+}
+
+chosenWine([
+    { name: "Wine A", price: 8.99 },
+    { name: "Wine 32", price: 13.99 },
+    { name: "Wine 9", price: 10.99 },
+]); //➞ "Wine 9"
+
+chosenWine([{ name: "Wine A", price: 8.99 }]); //➞ "Wine A"
+
+chosenWine([]); //➞ null
