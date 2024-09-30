767f6ea34d04532db39ea815d7aab317ce57c8c0
Jafar Khan <profile.mdjafarkhan@gmail.com>
2024-09-30 11:53:23 +0600

    array, iteration, conditions

--- 24. Check if bigram exist /index.js
+++ 24. Check if bigram exist /index.js
@@ -24,9 +24,6 @@ function returnUnique(arr) {
 
     console.log(str);
 }
-
 returnUnique([1, 9, 8, 8, 7, 6, 1, 6]);
-
 returnUnique([5, 5, 2, 4, 4, 4, 9, 9, 9, 1]);
-
 returnUnique([9, 5, 6, 8, 7, 7, 1, 1, 1, 1, 1, 9, 8]);
