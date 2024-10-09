aeb4c29631f63e57fa5a2246d423e4cf8cc07f30
Jafar Khan <profile.mdjafarkhan@gmail.com>
2024-10-09 12:49:21 +0600

    math, conditions, object, strings

--- 34. Human language understandable calculator/index.js
+++ 34. Human language understandable calculator/index.js
@@ -1,12 +1,12 @@
 /*
-Create a function that outputs the result of a math expression.
+Create a function that outputs the result of a math expression in word.
 
 Examples
-wordedMath("One plus one") ➞ 2
+wordedMath("One plus fIVe") ➞ 'Six'
 
-wordedMath("zero Plus one") ➞ 1
+wordedMath("zErO Plus one") ➞ 'One'
 
-wordedMath("one minus one") ➞ 0
+wordedMath("Nine plus nine") ➞ 'Eighteen
 Notes
 Expect only the operations plus and minus.
 Expect to only get numbers and answers from 0 to 9.
@@ -15,6 +15,7 @@ Expect to only get numbers and answers from 0 to 9.
 function wordedMath(sentence) {
     sentence = sentence.split(" ");
     const numbers = {
+        zero: 0,
         one: 1,
         two: 2,
         three: 3,
@@ -24,31 +25,40 @@ function wordedMath(sentence) {
         seven: 7,
         eight: 8,
         nine: 9,
-        zero: 0,
+        ten: 10,
+        eleven: 11,
+        twelve: 12,
+        thirteen: 13,
+        fourteen: 14,
+        fifteen: 15,
+        sixteen: 16,
+        seventeen: 17,
+        eighteen: 18,
     };
 
+    let words = Object.keys(numbers);
+    let result;
     switch (sentence[1].toLowerCase()) {
         case "plus":
             {
-                console.log(
+                result =
                     numbers[sentence[0].toLowerCase()] +
-                        numbers[sentence[2].toLowerCase()]
-                );
+                    numbers[sentence[2].toLowerCase()];
             }
             break;
         case "minus":
             {
-                console.log(
+                result =
                     numbers[sentence[0].toLowerCase()] -
-                        numbers[sentence[2].toLowerCase()]
-                );
+                    numbers[sentence[2].toLowerCase()];
             }
             break;
     }
+
+    console.log(words[result][0].toLocaleUpperCase() + words[result].slice(1));
 }
 
-wordedMath("One plus one");
-
-wordedMath("zero Plus one");
-
-wordedMath("one minus one");
+wordedMath("zero Plus one"); //➞ "One"
+wordedMath("one minus one"); //➞ "Zero"
+wordedMath("Nine plus nine"); //➞ "Eighteen"
+wordedMath("seVen plus Six"); //➞ "Thirteen"
